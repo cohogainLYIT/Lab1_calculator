169451a44386c27f85ca45db606aa233f94bb081
L00169829 <l00169829@student.lyit.ie>
2021-10-28 20:51:00 +0100

    calculator code and tests

--- src/main/java/simple_calculator.java
+++ src/main/java/simple_calculator.java
@@ -1,9 +1,36 @@
 
 public class simple_calculator {
+	
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public static int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public static int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public static int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Simple calculator yuupp");
+		
+		//Call and print result of add function
+		System.out.println(add(3,7));
+		
+		//Call and print result of subtract function
+		//System.out.println(subtract(15,5));
+		
+		//Call and print result of multiply function
+		//System.out.println(multiply(2,4));
+		
+		//Call and print result of divide function
+		//System.out.println(divide(12,6));
 		
 
 	}
--- src/test/java/simple_calc_test.java
+++ src/test/java/simple_calc_test.java
@@ -7,14 +7,39 @@ import org.junit.Test;
 
 public class simple_calc_test {
 
+	@SuppressWarnings("unlikely-arg-type")
 	@Test
-	public void test() {
-		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-		
+	public void test_add() {
+		simple_calculator.add(3,6);
+		equals(9);
+	}
+	
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	public void test_subtract() {
+		simple_calculator.add(9,6);
+		equals(3);
+	}
+	
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	public void test_multiply() {
+		simple_calculator.add(3,6);
+		equals(18);
+	}
+	
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	public void test_divide() {
+		simple_calculator.add(9,3);
+		equals(3);
+	}
+	
+	
+	public void test_main() {
 		simple_calculator.main(null);
+		equals(10);
 		
-		assertEquals("Simple calculator" + System.getProperty("line.separator"), outContent.toString());
 	}
 
 }
