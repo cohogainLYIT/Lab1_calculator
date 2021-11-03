9c40cbcc9a1a0d8254e817a4afdeeca81f9c7b75
L00169829 <l00169829@student.lyit.ie>
2021-11-03 00:42:27 +0000

    Changed to JDK 11

--- src/test/java/calc_package/calculator_unit_tests.java
+++ src/test/java/calc_package/calculator_unit_tests.java
@@ -3,23 +3,20 @@
  */
 package calc_package;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+
 
 /**
  * @author ciara
  *
  */
-public class simple_calc_lab1_test {
+public class calculator_unit_tests {
 
 	/**
 	 * Test method for {@link simple_calculator#add(int, int)}.
 	 */
-	@SuppressWarnings("unlikely-arg-type")
 	@Test
 	public void testAdd() {
 		assertEquals(9,simple_calc_lab1.add(3,6));
@@ -29,7 +26,6 @@ public class simple_calc_lab1_test {
 	/**
 	 * Test method for {@link simple_calculator#subtract(int, int)}.
 	 */
-	@SuppressWarnings("unlikely-arg-type")
 	@Test
 	public void testSubtract() {
 		assertEquals(4,simple_calc_lab1.subtract(10,6));
@@ -38,7 +34,6 @@ public class simple_calc_lab1_test {
 	/**
 	 * Test method for {@link simple_calculator#multiply(int, int)}.
 	 */
-	@SuppressWarnings("unlikely-arg-type")
 	@Test
 	public void testMultiply() {
 		assertEquals(18,simple_calc_lab1.multiply(3,6));
@@ -47,7 +42,6 @@ public class simple_calc_lab1_test {
 	/**
 	 * Test method for {@link simple_calculator#divide(int, int)}.
 	 */
-	@SuppressWarnings("unlikely-arg-type")
 	@Test
 	public void testDivide() {
 		assertEquals(8,simple_calc_lab1.divide(12,6));
@@ -56,7 +50,6 @@ public class simple_calc_lab1_test {
 	/**
 	 * Test method for {@link simple_calculator#main(java.lang.String[])}.
 	 */
-	@SuppressWarnings("unlikely-arg-type")
 	@Test
 	public void testMain() {
 		equals("Testing");
